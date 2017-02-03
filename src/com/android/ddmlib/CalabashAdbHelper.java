6f08699f5110dc9d76d1bee129af3b2dd236abf5
Rodrigo Medeiros <rodrigo.medeiros@concrete.com.br>
2017-02-03 19:02:18 -0200

    Adds optional variable to run adb on a external server. Defaults to localhost

--- src/com/android/ddmlib/CalabashAdbHelper.java
+++ src/com/android/ddmlib/CalabashAdbHelper.java
@@ -4,9 +4,14 @@ import java.net.InetSocketAddress;
 
 public class CalabashAdbHelper {
     public static final int ADB_PORT;
+    public static final String ADB_SERVER;
 
     private static final int DEFAULT_ADB_PORT = 5037;
     private static final String ENV_VAR_ADB_PORT = "ANDROID_ADB_SERVER_PORT";
+
+    private static final String DEFAULT_ADB_SERVER = "127.0.0.1";
+    private static final String ENV_VAR_ADB_REMOTE_SERVER = "ANDROID_ADB_REMOTE_SERVER";
+
 
     static {
         int port = DEFAULT_ADB_PORT;
@@ -16,10 +21,17 @@ public class CalabashAdbHelper {
             port = Integer.parseInt(envPort);
         }
 
+        String server = DEFAULT_ADB_SERVER;
+        String envServer = System.getenv(ENV_VAR_ADB_REMOTE_SERVER);
+        if (envServer != null && !"".equals(envServer)) {
+            server = envServer;
+        }
+
         ADB_PORT = port;
+        ADB_SERVER = server;
     }
 
     public static RawImage getFrameBuffer(Device device) throws Exception {
-        return AdbHelper.getFrameBuffer(new InetSocketAddress("127.0.0.1", ADB_PORT), device);
+        return AdbHelper.getFrameBuffer(new InetSocketAddress(ADB_SERVER, ADB_PORT), device);
     }
 }
